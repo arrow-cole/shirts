72b4076d9efa27ec8fd650af3db5cc261809bc3e
Aaron Cole <145709167+arrow-cole@users.noreply.github.com>
2024-06-30 17:22:17 -0400

    Update script.js

--- BACKEND/script.js
+++ BACKEND/script.js
@@ -5,8 +5,8 @@ document.getElementById('enter-store-button').addEventListener('click', function
 
 document.getElementById('submit-password-button').addEventListener('click', function() {
   var password = document.getElementById('password-input').value;
-  if (password === 'testing') { // Replace 'your_password_here' with the actual password
-      window.location.href = 'store.html';
+  if (password === 'westgate') { // Replace 'your_password_here' with the actual password
+      window.location.href = 'FRONTEND/index.html';
   } else {
       document.getElementById('error-message').classList.remove('hidden');
   }
